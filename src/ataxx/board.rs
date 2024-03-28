da7a10b7e0a6ba899921288ca0e8cb064fc45bae
Rak Laptudirm <rak@laptudirm.com>
2024-03-29 02:18:19 +0530

    chore: refactor board.rs

--- src/ataxx/board.rs
+++ src/ataxx/board.rs
@@ -1,4 +1,4 @@
-// Copyright © 2023 Rak Laptudirm <rak@laptudirm.com>
+// Copyright © 2024 Rak Laptudirm <rak@laptudirm.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,22 +23,24 @@ pub struct Board {
 }
 
 impl Board {
-    // MAX_PLY is the maximum number of plys than can be in a game.
+    /// MAX_PLY is the maximum number of plys than can be in a game.
+    /// Actually there can be more plys but this is a nice upper bound to
+    /// use for the length of the board's position history.
     const MAX_PLY: usize = 1024;
 
     const fn current_pos(&self) -> &Position {
         &self.history[self.current]
     }
 
-    // Position returns a copy of the current Position on the Board.
+    /// position returns a copy of the current Position on the Board.
     pub fn position(&self) -> Position { self.history[self.current] }
 
-    // side_to_move returns the current Color to move on the Board.
+    /// side_to_move returns the current Color to move on the Board.
     pub fn side_to_move(&self) -> Color {
         self.current_pos().side_to_move
     }
 
-    // Checksum returns a semi-unique Hash to identify the Position by.
+    /// checksum returns a semi-unique Hash to identify the Position by.
     pub fn checksum(&self) -> Hash {
         self.current_pos().checksum
     }
@@ -47,26 +49,31 @@ impl Board {
     // Reimplementation of Position's methods //
     ////////////////////////////////////////////
 
+    /// bitboard returns the BitBoard associated to the piece configuration of the given Color.
     pub const fn bitboard(&self, color: Color) -> BitBoard {
         self.current_pos().bitboard(color)
     }
 
+    /// at returns the Color of the piece present at the given Square.
     pub const fn at(&self, sq: Square) -> Color {
         self.current_pos().at(sq)
     }
 }
 
 impl Board {
+    /// make_move plays the given Move on the Board and updates state accordingly.
     pub fn make_move(&mut self, m: Move) {
         self.history[self.current + 1] = self.current_pos().after_move(m);
         self.current += 1;
     }
 
+    /// undo_move un-plays the last played Move on the Board.
     pub fn undo_move(&mut self) {
         self.current -= 1;
     }
 }
 
+// Implementation for converting a FEN into a Board.
 impl From<&FEN> for Board {
     fn from(fen: &FEN) -> Self {
         let mut board = Board {
@@ -80,13 +87,20 @@ impl From<&FEN> for Board {
 }
 
 impl Board {
+    /// generate_moves generates the legal moves in the current Position and returns a MoveList
+    /// containing all the moves. It is a wrapper on top of the more general generate_moves_into.
     pub fn generate_moves(&self) -> MoveList {
         self.current_pos().generate_moves()
     }
+
+    /// generate_moves_into generates all the legal moves in the current Position and adds them
+    /// to the given movelist. The type of the movelist must implement the MoveStore trait.
     pub fn generate_moves_into<T: MoveStore>(&self, movelist: &mut T) {
         self.current_pos().generate_moves_into(movelist);
     }
 
+    /// count_moves returns the number of legal moves in the current Position. It is faster than
+    /// calling generate_moves or generate_moves_into and then finding the length of the movelist.
     pub fn count_moves(&self) -> usize {
         self.current_pos().count_moves()
     }
@@ -100,6 +114,7 @@ pub struct Position {
 }
 
 impl Position {
+    /// new creates a new Position with the given piece configurations and side to move.
     pub fn new(white: BitBoard, black: BitBoard, stm: Color) -> Position {
         Position {
             bitboards: [white, black],
@@ -108,10 +123,12 @@ impl Position {
         }
     }
 
+    /// bitboard returns the BitBoard associated with the piece configuration of the given Color.
     pub const fn bitboard(&self, color: Color) -> BitBoard {
         self.bitboards[color as usize]
     }
 
+    /// put puts the given piece represented by its Color on the given Square.
     pub fn put(&mut self, sq: Square, color: Color) {
         match color {
             Color::White => self.bitboards[Color::White as usize].insert(sq),
@@ -120,6 +137,7 @@ impl Position {
         };
     }
 
+    /// at returns the Color of the piece present on the given Square.
     pub const fn at(&self, sq: Square) -> Color {
         if self.bitboard(Color::White).contains(sq) {
             Color::White
@@ -130,41 +148,57 @@ impl Position {
         }
     }
 
+    /// is_game_over checks if the game is over, i.e. is a win or a draw.
     pub fn is_game_over(&self) -> bool {
         let white = self.bitboard(Color::White);
         let black = self.bitboard(Color::Black);
         white | black == BitBoard::UNIVERSE || white == BitBoard::EMPTY || black == BitBoard::EMPTY
     }
 
+    /// winner returns the Color which has won the game. It returns Color::None if the game is a
+    /// draw. If is_game_over is false, the behaviour of this function is undefined.
     pub fn winner(&self) -> Color {
+        debug_assert!(self.is_game_over());
+
         let white = self.bitboard(Color::White);
         let black = self.bitboard(Color::Black);
 
         if white == BitBoard::EMPTY {
+            // White lost all its pieces, black won.
             return Color::Black;
         } else if black == BitBoard::EMPTY {
+            // Black lost all its pieces, white won.
             return Color::White;
-        } else if white | black != BitBoard::UNIVERSE {
-            return Color::None;
         }
+
+        debug_assert!(white | black == BitBoard::UNIVERSE);
+
+        // All the squares are occupied by pieces. Victory is decided by
+        // which Color has the most number of pieces on the Board.
 
         let white_n = white.cardinality();
         let black_n = black.cardinality();
 
         if white_n > black_n {
+            // White has more pieces, white wins.
             Color::White
         } else if black_n > white_n {
+            // Black has more pieces, black wins.
             Color::Black
         } else {
+            // Equal number of pieces, draw.
             Color::None
         }
     }
 }
 
 impl Position {
+    /// after_move returns a new Position which occurs when the given Move is played on the
+    /// current Position. Its behaviour is undefined if the given Move is illegal.
     pub fn after_move(&self, m: Move) -> Position {
         let stm = self.side_to_move;
 
+        // A pass move is a do nothing move; just change the side to move.
         if m == Move::PASS {
             return Position::new(self.bitboard(Color::White), self.bitboard(Color::Black), !stm);
         }
@@ -172,7 +206,7 @@ impl Position {
         let stm_pieces = self.bitboard(stm);
         let xtm_pieces = self.bitboard(!stm);
 
-        let captured = BitBoard::SINGLES[m.target() as usize] & xtm_pieces;
+        let captured = BitBoard::single(m.target()) & xtm_pieces;
 
         // Move the captured pieces from xtm to stm.
         let new_xtm = xtm_pieces ^ captured;
@@ -195,82 +229,119 @@ impl Position {
 }
 
 impl Position {
+    /// generate_moves generates the legal moves in the current Position and returns a MoveList
+    /// containing all the moves. It is a wrapper on top of the more general generate_moves_into.
     pub fn generate_moves(&self) -> MoveList {
         let mut movelist = MoveList::new();
         self.generate_moves_into(&mut movelist);
         movelist
     }
+
+    /// generate_moves_into generates all the legal moves in the current Position and adds them
+    /// to the given movelist. The type of the movelist must implement the MoveStore trait.
     pub fn generate_moves_into<T: MoveStore>(&self, movelist: &mut T) {
         let stm = self.bitboard(self.side_to_move);
         let xtm = self.bitboard(!self.side_to_move);
 
+        // Pieces can only move to unoccupied Squares.
         let allowed = !(stm | xtm);
 
         let mut single = BitBoard::EMPTY;
         for piece in stm {
-            single |= BitBoard::SINGLES[piece as usize];
+            // All single moves to a single Square are equivalent, so a single
+            // BitBoard is sufficient to keep track of them all and cast out duplicates.
+            // An intersection with the allowed BitBoard is done once at the end.
+            single |= BitBoard::single(piece);
 
-            let double = BitBoard::DOUBLES[piece as usize] & allowed;
+            // There may be multiple jump moves to a single Square, so they need to be
+            // verified (& allowed) and serialized into the movelist immediately.
+            let double = BitBoard::double(piece) & allowed;
             for target in double {
                 movelist.push(Move::new(piece, target));
             }
         }
 
+        // Serialize the single moves into the movelist.
         single &= allowed;
         for target in single {
             movelist.push(Move::new_single(target));
         }
 
+        // If there are no legal moves and the game isn't over, a pass move is possible.
         if movelist.len() == 0 && !self.is_game_over() {
             movelist.push(Move::PASS);
         }
     }
 
+    /// count_moves returns the number of legal moves in the current Position. It is faster than
+    /// calling generate_moves or generate_moves_into and then finding the length of the movelist.
     pub fn count_moves(&self) -> usize {
         let mut moves: usize = 0;
 
         let stm = self.bitboard(self.side_to_move);
         let xtm = self.bitboard(!self.side_to_move);
 
+        // Pieces can only move to unoccupied Squares.
         let allowed = !(stm | xtm);
 
         let mut single = BitBoard::EMPTY;
         for piece in stm {
-            single |= BitBoard::SINGLES[piece as usize];
+            // All single moves to a single Square are equivalent, so a single
+            // BitBoard is sufficient to keep track of them all and cast out duplicates.
+            // An intersection with the allowed BitBoard is done once at the end.
+            single |= BitBoard::single(piece);
 
-            let double = BitBoard::DOUBLES[piece as usize] & allowed;
-            moves += double.cardinality() as usize;
+            // There may be multiple jump moves to a single Square, so they need to be
+            // verified (& allowed) and counted into the Position total immediately.
+            let double = BitBoard::double(piece) & allowed;
+            moves += double.cardinality();
         }
 
-        moves + (single & allowed).cardinality() as usize
+        // Count the number single moves in the Position.
+        moves += (single & allowed).cardinality();
+
+        // If there are no legal moves and the game isn't over, a pass move is possible.
+        if moves == 0 && !self.is_game_over() {
+            moves += 1;
+        }
+
+        moves
     }
 }
 
+/// PositionParseErr represents an error encountered while parsing
+/// the given FEN position field into a valid Position.
 #[derive(Debug)]
 pub enum PositionParseErr {
-    JumpTooLong,
-    InvalidColorIdent,
-    FileDataIncomplete,
-    TooManyFields,
+    JumpTooLong,        // A jump spec was too big for the current rank.
+    InvalidColorIdent,  // Invalid character in Square spec.
+    FileDataIncomplete, // Insufficient number of Square spec entries.
+    TooManyFields,      // More fields in string spec than Ranks.
 }
 
+// FromStr implements parsing of the position field in a FEN.
 impl FromStr for Position {
     type Err = PositionParseErr;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut position = Position::new(BitBoard::EMPTY, BitBoard::EMPTY, Color::None);
 
+        // Spilt the position spec by the Ranks which are separated by '/'.
         let ranks: Vec<&str> = s.split('/').collect();
 
         let mut rank = Ok(Rank::Seventh);
         let mut file = Ok(File::A);
+
+        // Iterate over the Ranks in the string spec.
         for rank_data in ranks {
             // Rank pointer ran out, but data carried on.
             if rank.is_err() {
                 return Err(PositionParseErr::TooManyFields);
             }
 
+            // Iterate over the Square specs in the Rank spec.
             for data in rank_data.chars() {
+                // Check if a jump spec was too big and we landed on an invalid File.
                 if file.is_err() {
                     return Err(PositionParseErr::JumpTooLong);
                 }
@@ -279,6 +350,7 @@ impl FromStr for Position {
                     'O' | 'o' | 'w' => position.put(square, Color::White),
                     'X' | 'x' | 'b' => position.put(square, Color::Black),
 
+                    // Numbers represent jump specs to jump over empty squares.
                     '1'..='8' => {
                         file = File::try_from(file.unwrap() as usize + data as usize - '1' as usize);
                         if file.is_err() {
@@ -289,6 +361,7 @@ impl FromStr for Position {
                     _ => return Err(PositionParseErr::InvalidColorIdent),
                 }
 
+                // On to the next Square spec in the Rank spec.
                 file = File::try_from(file.unwrap() as usize + 1);
             }
 
@@ -303,6 +376,7 @@ impl FromStr for Position {
             file = Ok(File::A);
         }
 
+        // Calculate the Hash value for the Position.
         position.checksum = Hash::new(
             position.bitboard(Color::White).0,
             position.bitboard(Color::Black).0,
@@ -323,9 +397,11 @@ impl fmt::Display for Position {
                 string_rep += &format!("{} ", board.at(square));
             }
 
+            // Append the rank marker.
             string_rep += &format!(" {} \n ", rank);
         }
 
+        // Append the file markers.
         string_rep += "a b c d e f g\n";
 
         write!(f, "{}\n", string_rep).unwrap();
