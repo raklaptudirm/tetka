e57783aace993f1521926fc0119747f88f7a7f81
Rak Laptudirm <rak@laptudirm.com>
2024-05-25 21:32:01 +0530

    chore: add perft and related tests

--- ataxx/src/lib.rs
+++ ataxx/src/lib.rs
@@ -3,6 +3,7 @@
 // without their parent namespace.
 pub use self::bitboard::*;
 pub use self::hash::*;
+pub use self::perft::*;
 pub use self::piece::*;
 pub use self::position::*;
 pub use self::r#move::*;
@@ -12,6 +13,7 @@ pub use self::square::*;
 mod bitboard;
 mod hash;
 mod r#move;
+mod perft;
 mod piece;
 mod position;
 mod square;
---
+++ ataxx/tests/perft.rs
@@ -0,0 +1,37 @@
+use ataxx::{perft, Position};
+use std::str::FromStr;
+
+macro_rules! perft_test {
+    ($name:ident $pos:literal $depth:literal $nodes:literal) => {
+        #[test]
+        fn $name() {
+            let position = Position::from_str($pos).unwrap();
+            assert_eq!(perft::<true, true>(position, $depth), $nodes)
+        }
+    };
+}
+
+// Tests taken from libataxx
+
+perft_test!(position_01_x "7/7/7/7/7/7/7 x 0 1" 4 0);
+perft_test!(position_01_o "7/7/7/7/7/7/7 o 0 1" 4 0);
+perft_test!(position_02_x "x5o/7/7/7/7/7/o5x x 0 1" 5 4752668);
+perft_test!(position_02_o "x5o/7/7/7/7/7/o5x o 0 1" 5 4752668);
+perft_test!(position_03_x "x5o/7/2-1-2/7/2-1-2/7/o5x x 0 1" 5 2266352);
+perft_test!(position_03_o "x5o/7/2-1-2/7/2-1-2/7/o5x o 0 1" 5 2266352);
+perft_test!(position_04_x "x5o/7/2-1-2/3-3/2-1-2/7/o5x x 0 1" 5 2114588);
+perft_test!(position_04_o "x5o/7/2-1-2/3-3/2-1-2/7/o5x o 0 1" 5 2114588);
+perft_test!(position_05_x "x5o/7/3-3/2-1-2/3-3/7/o5x x 0 1" 5 3639856);
+perft_test!(position_05_o "x5o/7/3-3/2-1-2/3-3/7/o5x o 0 1" 5 3639856);
+perft_test!(position_06_x "7/7/7/7/ooooooo/ooooooo/xxxxxxx x 0 1" 5 452980);
+perft_test!(position_06_o "7/7/7/7/ooooooo/ooooooo/xxxxxxx o 0 1" 4 452980);
+perft_test!(position_07_x "7/7/7/7/xxxxxxx/xxxxxxx/ooooooo x 0 1" 4 452980);
+perft_test!(position_07_o "7/7/7/7/xxxxxxx/xxxxxxx/ooooooo o 0 1" 5 452980);
+perft_test!(position_08_x "7/7/7/2x1o2/7/7/7 x 0 1" 5 4266992);
+perft_test!(position_08_o "7/7/7/2x1o2/7/7/7 o 0 1" 5 4266992);
+perft_test!(position_09_x "x5o/7/7/7/7/7/o5x x 100 1" 5 0);
+perft_test!(position_09_o "x5o/7/7/7/7/7/o5x o 100 1" 5 0);
+
+// TODO: Deal with disjointing blockers
+perft_test!(position_10_x "7/7/7/7/-------/-------/x5o x 0 1" 6 175); // 174 ^^
+perft_test!(position_10_o "7/7/7/7/-------/-------/x5o o 0 1" 6 175); // 174 ^^
