3d43977513a3929f21edb5b6ee5bb08cf1a00feb
Rak Laptudirm <rak@laptudirm.com>
2024-05-29 13:52:05 +0530

    chore: fix spacing

--- src/mcts/params.rs
+++ src/mcts/params.rs
@@ -1,32 +1,32 @@
 use std::f64::consts::SQRT_2;
 
 pub struct Params {
-	cpuct: Param,
+    cpuct: Param,
 }
 
 impl Params {
-	pub fn new() -> Params {
-		Params {
-			cpuct: Param::new(SQRT_2, 1.0, 10.0),
-		}
-	}
-	pub fn cpuct(&self) -> f64 {
-		self.cpuct.val
-	}
+    pub fn new() -> Params {
+        Params {
+            cpuct: Param::new(SQRT_2, 1.0, 10.0),
+        }
+    }
+    pub fn cpuct(&self) -> f64 {
+        self.cpuct.val
+    }
 }
 
 pub struct Param {
-	val: f64,
-	min: f64,
-	max: f64,
+    val: f64,
+    min: f64,
+    max: f64,
 }
 
 impl Param {
-	pub fn new(val: f64, min: f64, max: f64) -> Param {
-		Param{val, min, max}
-	}
+    pub fn new(val: f64, min: f64, max: f64) -> Param {
+        Param { val, min, max }
+    }
 
-	pub fn set(&mut self, val: f64) {
-		self.val = val;
-	}
+    pub fn set(&mut self, val: f64) {
+        self.val = val;
+    }
 }
