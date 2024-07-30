edc54dc9ccaede9666fb612414798ff427079114
Rak Laptudirm <rak@laptudirm.com>
2024-07-30 19:46:32 +0530

    unused use ahhh

--- games/src/ataxx/square.rs
+++ games/src/ataxx/square.rs
@@ -11,8 +11,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use std::fmt;
 use std::str::FromStr;
-use std::{fmt, mem};
 
 use thiserror::Error;
 
