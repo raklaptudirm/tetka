e4f077d39975f1f7ad38ac9c76e011aafa96b6c3
Rak Laptudirm <rak@laptudirm.com>
2024-09-14 17:07:08 +0530

    chore: fix universe bb

--- games/src/chess/bitboard.rs
+++ games/src/chess/bitboard.rs
@@ -24,7 +24,7 @@ bitboard_type! {
 
         // BitBoards representing the null and the universe sets.
         Empty = Self(0);
-        Universe = Self(0xfffffffffffff);
+        Universe = Self(0xfffffffffffffff);
 
         // BitBoards containing the squares of the first file and the first rank.
         FirstFile = Self(0x0101010101010101);
