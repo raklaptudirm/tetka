0915648bf524124c40904522e92f871035211e7a
Rak Laptudirm <rak@laptudirm.com>
2025-05-20 18:54:37 +0530

    chore: comments and licenses

--- games/src/games/mod.rs
+++ games/src/games/mod.rs
@@ -1,3 +1,15 @@
+//! This module provides implementations of the APIs defined in `interface` of
+//! various popular games.
+//!
+//! Each module represents the most general version of a single game. For
+//! example, the chess module provides a implementation for DFRC chess, which
+//! is a superset of standard chess.
+//!
+//! The common interfaces defined in `interface` are impelemnted for each game
+//! in this list, along with other game-specific functionality, such as
+//! types for working with castling rights in chess. Refer to the documentation
+//! of `interface` for more information about the common API.
+
 pub mod ataxx;
 pub mod chess;
 pub mod isolation;
--- games/src/lib.rs
+++ games/src/lib.rs
@@ -1,3 +1,13 @@
+//! tetka-games is a general board game library which provides generic
+//! interfaces for creating game agnostic but performant software.
+//!
+//! The three modules provide different functionality of the library:
+//! - `interface`: Provides the generic game agnostic interface for working
+//!    with the logic and boards of different games in a single codebase.
+//! - `common`: Provides game agnostic utilities which build on top of the apis
+//!    provided by `interface`. This includes a `perft` implementation.
+//! - `games`: Provides board representations for specific games.
+
 pub mod common;
 pub mod games;
 pub mod interface;
