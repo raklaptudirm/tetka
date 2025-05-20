305ce126eb9ead628a5aa9f335e732cb142eaca2
Rak Laptudirm <rak@laptudirm.com>
2025-05-20 19:21:51 +0530

    chore: ignore time consuming chess tests

--- games/src/tests/games/chess.rs
+++ games/src/tests/games/chess.rs
@@ -5,6 +5,7 @@ use std::str::FromStr;
 macro_rules! perft_test {
     ($name:ident $pos:literal $depth:literal $nodes:literal) => {
         #[test]
+        #[ignore]
         fn $name() {
             let position = Position::from_str($pos).unwrap();
             assert_eq!(perft::<true, true, _>(position, $depth), $nodes)
